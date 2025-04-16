f581f8814627b13a8becb3aeeb5dd31a6f2a9d8f
Fa'iq Hammam <hammamfaiq@protonmail.com>
2025-04-16 13:04:48 +0700

    delete some file

--- tests/test_utils.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from src.data.indicators import TechnicalIndicators
-
-def test_rsi_output_shape():
-    data = np.random.rand(100) * 100
-    rsi = TechnicalIndicators.calculate_rsi(data)
-    assert len(rsi) == len(data)
